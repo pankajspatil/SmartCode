b869742b93eb78536d1927d8cc2eb1b2fbbaeec5
pankaj <Pankaj@Patil-PC>
2015-07-04 09:29:43 +0530

    Session Filter and Web.xml

---
+++ DoctorsOnline/src/com/org/doctorsonline/generic/SessionFilter.java
@@ -0,0 +1,59 @@
+package com.org.doctorsonline.generic;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class SessionFilter
+ */
+@WebFilter("/SessionFilter")
+public class SessionFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public SessionFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	@Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("user") == null) {
+            response.sendRedirect(request.getContextPath() + "/pages/login.jsp"); // No logged-in user found, so redirect to login page.
+        } else {
+            chain.doFilter(req, res); // Logged-in user found, so just continue request.
+        }
+    }
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
